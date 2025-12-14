1b356fdca5277bdbd84d56fe308d48d0eb7c3394
Meheraj-Hossain-Shanto <meherazshanto20@gmail.com>
2025-12-14 13:57:20 +0600

    Some JS code

---
+++ hello.js
@@ -0,0 +1,89 @@
+// const num =4334
+// let nam="Meheraj Hossain Shanto"
+// var id="21CSE013"
+
+// console.table([num,nam,id])
+
+// console.log(typeof(num))
+// console.log(typeof(nam))
+// console.log(typeof(id))
+
+// alert ("Bal a website")
+// document.write("Meheraj Hosssain Shanto<Br/>")
+
+// document.write("01521257043")
+// const student={
+//     nam: "Meheraj Hossain Shanto",
+//     age: 28,
+//     id: "21CSE013",
+//     email:"meherazshanto20@gmail.com",
+// };
+
+// console.log(student.nam)
+
+// student["nam"]="Mehedi Hasan Siam"
+// console.log(student.nam)
+// let age=18;
+
+// if (age%2==0){
+//     console.log("Good");
+// }else{
+//     console.log("bad");
+// }
+// let dispaly="12"
+
+// if(dispaly==="1"){
+//     console.log("white");
+// }else{
+//     console.log("dark");
+// }
+// const prompt = require('prompt-sync')();
+// let nam=prompt("Enter your name:");
+// console.log(nam);
+// document.getElementById("d").innerText="shanto"
+// let n= prompt("Enter a number a :")
+// n=Number.parseInt(n)
+// for (let i=1;i<=n;i++){
+//     console.log(i);
+// }
+// let arr=[23,35,67,32,87,55,50]
+// console.log(arr)
+// arr.push(15)
+// console.log(arr)
+// console.log(arr.length)
+// delete(arr[2])
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// let compare = (a, b)=>{      //array sort function with sort-building function 
+//     return a-b  
+// }
+// let arr=[24,2,78,23,89,112,45,6]
+// arr.sort(compare)//this order by alphabet 
+// console.log(arr)
+
+// let arr=[23,56,2,1,766,45,67]
+// arr.map((value)=>{
+//     console.log(value)
+// })
+// console.error("This is a website error")
+// console.log(console)
+// alert("It is high risk website ")
+// var a=prompt("Enter a number :")
+// console.log(a)
+
+// let arr=[23,65,12,86,25,72,32]
+
+// let a=arr.filter((item)=>{
+//     return item>=45
+// })
+// console.log(a)
+// console.warn(a)
+
+// let loatNumber=3.56564
+// loatNumber = Math.round(loatNumber)
+// loatNumber = Math.ceil(loatNumber)
+// loatNumber = Math.floor(loatNumber)
+// console.log(loatNumber);
+// let date = new Date('11-15-2025')
+// console.log(date)
